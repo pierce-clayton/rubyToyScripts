0deac67e266bdad15971a73c47e0b15243874083
technicholy <46853031+technicholy@users.noreply.github.com>
2020-09-02 00:01:36 -0700

    refactored change counter to class

--- changeCounter.rb
+++ changeCounter.rb
@@ -1,25 +1,28 @@
 #!/usr/bin/ruby
-# global var for the till in use. could be any set of denominations
-$till = [10000,5000,2000,1000,500,100,25,10,5,1]
-# returns a string of all the denominations included for that much change. 
-# entered number must be whole number, dollars * 100
-def changeCounter(change)    
-    toReturn = []
-    $till.each_with_index do |denom, index|
-        toReturn[index] = change / denom
-        change = change - toReturn[index] * denom
-    end   
-    return toReturn
-end
 
-# puts a string of what the change consists of per denomination, like a change dispenser.
-def changeMachine(changeTill)
-    retStr = "The change is"
-    changeTill.each_with_index do |denom, index|
-        denom > 0 ? (retStr << ", #{denom} x $#{$till[index] * 0.01}") :
-        nil
+# takes input in cents and counts change back by denomination
+class ChangeManager
+    def initialize
+        @till = [10000,5000,2000,1000,500,100,25,10,5,1] # USD * 100
+        @change = ARGV[0].to_i
+        @countOut = []
+        counter # run the counter to fill the countOut array
     end
-    puts retStr
+    def counter
+        @till.each_with_index do |denom, index|
+            @countOut[index] = @change / denom
+            @change = @change - @countOut[index] * denom
+        end
+    end
+    def machine
+        delivery = "The change is"
+        @countOut.each_with_index do |denom, index|
+            denom > 0 ? (delivery << ", #{denom} x $#{'%.2f' % (@till[index] * 0.01)}") : nil
+        end
+        puts delivery
+    end
 end
 
-changeMachine(changeCounter(ARGV[0].to_i))
+
+transactionBlah = ChangeManager.new
+transactionBlah.machine
